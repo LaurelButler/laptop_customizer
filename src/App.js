528b898cb2f83ed5570599c60b5d0eede19cf38e
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-03-22 16:57:57 -0400

    got features working

--- src/App.js
+++ src/App.js
@@ -38,42 +38,20 @@ class App extends Component {
   }
 
   render() {
-    const summary = Object.keys(this.state.selected)
-          .map(key => <div className="summary__option" key={key}>
-            <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.state.selected[key].name}</div>
-            <div className="summary__option__cost">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.state.selected[key].cost) }
-            </div>
-        </div>)
-
-    const total = Object.keys(this.state.selected)
-          .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
-
-
-    
 
     return ( <div className="App">
         <Header /> 
         <main>
           <Specs cookies={this.props.slimjim} selected={this.state.selected}/>
-          <Features />
+          <Features icecream={this.state.selected}/>
 
         
           
     
-          <section className="main__summary">
-            <h3>NEW GREENLEAF 2018</h3>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Your Price: </div>
-              <div className="summary__total__value">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(total) }
-              </div>
-            </div>
-          </section>
+          
+            
+           
+            
         </main>
       </div>
    );
--- src/components/Features.js
+++ src/components/Features.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 class Features extends React.Component {
     render () {
+        const summary = Object.keys(this.props.icecream)
+            .map(key => <div className="summary__option" key={key}>
+                <div className="summary__option__label">{key}  </div>
+                <div className="summary__option__value">{this.props.icecream[key].name}</div>
+                <div className="summary__option__cost">
+                    {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+                        .format(this.props.icecream[key].cost)}
+                </div>
+                </div>
+            )
+        const total = Object.keys(this.props.icecream)
+            .reduce((acc, curr) => acc + this.props.icecream[curr].cost, 0);
+
         return (
-            <h3>hello all!</h3>
+            <section className="main__summary">
+            <h3>NEW GREENLEAF 2018</h3>
+            <div className="summary__total">
+                <div className="summary__total__label">Your Price: </div>
+                <div className="summary__total__value">
+                    {summary}
+                    {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+                        .format(total)}    
+                </div>     
+            </div>
+            </section>
         );
     };
 }
